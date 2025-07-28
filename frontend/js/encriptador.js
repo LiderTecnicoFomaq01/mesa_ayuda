b0b0c7fb33b1315900688cdf283e59d797093a2b
ivan_bermudez <ivandbermudezc@gmail.com>
2025-07-28 13:20:45 -0500

    estrellas cambiadas

--- frontend/js/encriptador.js
+++ frontend/js/encriptador.js
@@ -2,7 +2,7 @@
 const bcrypt = require('bcrypt');
 
 async function generarHash() {
-  const password = 'bermudez19971127';
+  const password = 'hola mundo';
   const hashedPassword = await bcrypt.hash(password, 10);
   console.log('Hashed password:', hashedPassword);
 }
